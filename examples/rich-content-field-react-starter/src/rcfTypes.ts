ca4adc45d197ac52dd5712a0744cd30edb41379b
Maria Chibel <maria.chibel@mywave.me>
2024-11-05 18:17:31 +1300

    fix: renamed file

---
+++ examples/rich-content-field-react-starter/src/rcfTypes.ts
@@ -0,0 +1,11 @@
+export type RCF<DataType> = {
+    id: string
+    contentType: string
+    data: DataType
+    isSelected: boolean
+  }
+  
+  export type RCFAnswer<DataType> = {
+    contentType: string
+    data: DataType
+  }
